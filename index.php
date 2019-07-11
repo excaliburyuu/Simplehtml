5bc803d509a7b043bcb212b66a0d6708b23c8afe
josesadriel <30821524+josesadriel@users.noreply.github.com>
2019-07-11 23:48:09 +0700

    Update index.php
    tambah load data

--- index.php
+++ index.php
@@ -10,9 +10,20 @@
 			Nama: <input type="text" placeholder="Masukkan Nama Anda.." name="nama"/><br/>
 			E-mail: <input type="mail" placeholder="Masukkan E-mail Anda.." name="email"/><br/>
 			Pekerjaan: <input type="text" placeholder="Masukkan Pekerjaan Anda.." name="pekerjaan"/><br/>
-			<input type="submit" name="kirim" value="Submit" />
+			<input type="submit" name="kirim" value="Submit" /> | <input type="submit" name="load" value="Load Data"/>
 		</form>
 		
+		<table border="1">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Nama</th>
+					<th>E-Mail</th>
+					<th>Pekerjaan</th>
+					<th>Tanggal</th>
+				</tr>
+			</thead>
+			<tbody>
 		<?php
 		    $host = "cobaazuredicoding.database.windows.net";
 		    $user = "josesadriel";
@@ -29,13 +40,30 @@
 					$nama = $_POST['nama'];
 					$email = $_POST['email'];
 					$job = $_POST['pekerjaan'];
-				
-					$sql = "INSERT INTO registrasi (nama, email, pekerjaan, tanggal) VALUES ('$nama', '$email', '$job', NOW())";
+					$waktu = date("Y-m-d");
+					$sql = "INSERT INTO registrasi (nama, email, pekerjaan, tanggal) VALUES ('$nama', '$email', '$job', '$waktu');";
 					$query = mysqli_query($conn, $sql);
 				} catch(Exception $e) {
 					echo "Gagal Submit!";
 				}
 			}
+			
+			if (isset($_POST['load'])) {
+				$sql = "SELECT * FROM registrasi;";
+				$query = mysqli_query($conn, $sql);
+				
+				while($data = mysqli_fetch_array($query)) {
+					echo "<tr>";
+
+					echo "<td>".$data['id']."</td>";
+					echo "<td>".$data['nama']."</td>";
+					echo "<td>".$data['email']."</td>";
+					echo "<td>".$data['pekerjaan']."</td>";
+					echo "<td>".$data['tanggal']."</td>";
+				}
+			}
 		?>
+			</tbody>
+		</table>
 	</body>
 </html>
