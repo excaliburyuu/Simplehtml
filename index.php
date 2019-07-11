26791d0b5d971eed0b10d1d48a420c376b085d49
josesadriel <30821524+josesadriel@users.noreply.github.com>
2019-07-11 23:56:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,11 +29,21 @@
 		    $user = "josesadriel";
 		    $pass = "Makanberger1";
 		    $db = "cobaazuredb";
-		    try {
-        		$conn = mysqli_connect($host, $user, $pass, $db);
-    		    } catch(Exception $e) {
-        		echo "Koneksi Gagal!";
-    		}
+		    // PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:cobaazuredicoding.database.windows.net,1433; Database = cobaazuredb", "josesadriel", "Makanberger1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "josesadriel@cobaazuredicoding", "pwd" => "Makanberger1", "Database" => "cobaazuredb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:cobaazuredicoding.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 		    
 			if (isset($_POST['kirim'])) {
 				try {
